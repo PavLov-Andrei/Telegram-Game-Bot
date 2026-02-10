29026ea0fcd54775e31cec014a52e2e73e44908f
Pavlov Andrei <payu2006@mail.ru>
2026-02-10 23:57:46 +0300

    new function for game.json

--- source/source.py
+++ source/source.py
@@ -53,20 +53,25 @@ def delete_pair(user_id: int) -> int: #удаляет пару, в случае 
 def create_game(first_user: int, second_user: int, game: str) -> None:
        with open('source/game.json', 'r', encoding = 'utf-8') as file:
               data = json.load(file)
-       data[str(first_user)+"|"+str(second_user)] = [game, 0, {}] #game, game_status{0: init, 1:in_game}, some_game_data
+       data[str(first_user)+"|"+str(second_user)] = [game, 0, {}] #game_name, game_status{0: init, 1:in_game}, some_game_data
        with open('source/game.json', 'w', encoding = 'utf-8') as file:
               json.dump(data, file, indent=4)
 
 def delete_game(user: int) -> int:
        with open('source/game.json', 'r', encoding = 'utf-8') as file:
               data = json.load(file)
-       second_user = 0 #типа не получилось удалить никакю игру
        for i in data:
-              if user in i: #это значение по типу "11111111111|22222222222", где цифры - id пользователей
+              if str(user) in i: #это значение по типу "11111111111|22222222222", где цифры - id пользователей
                      del data[i]
-                     second_user = int(i.replace(str(user), '').replace('|', ''))
-                     break
-              if second_user:
                      with open('source/game.json', 'w', encoding = 'utf-8') as file:
                             json.dump(data, file, indent=4)
-       return second_user
+                     return int(i.replace(str(user), '').replace('|', ''))
+       return 0 #типа не получилось удалить никакю игру
+
+def find_game(user: int) -> list:
+       with open('source/game.json', 'r', encoding = 'utf-8') as file:
+              data = json.load(file)
+       for i in data:
+              if str(user) in i:
+                     return [data[i][0], data[i][1]] #даём название
+       return ['0', -1]
